2a0f44bba14ef023ebbd03f5c322efc38270a959
sarahabimay <sarahabimay@gmail.com>
2015-10-01 16:46:07 +0100

    Third test required refactoring before and after to make it pass

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,11 +1,27 @@
 public class BowlingGame {
-    public int score = 0;
-
+    public int rolls[] = new int[21];
+    public int currentRoll = 0;
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for(int frame = 0; frame < 10; frame++){
+            if(isSpare(frameIndex)) {
+                score += 10 + rolls[frameIndex + 2];
+                frameIndex += 2;
+            }
+            else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+         }
         return score;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex+1] == 10;
     }
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -27,4 +27,16 @@ public class BowlingGameTest {
         rollMany(20, 1);
         assertEquals(20, g.score());
     }
+    @Test
+    public void testSpareGame(){
+        rollSpare();
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
 }
